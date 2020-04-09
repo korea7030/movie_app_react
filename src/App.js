e2da1bda033a63d3fb6ea2ea6c8150c5d1c53d3d
korea7030 <korea7030@naver.com>
2020-04-09 16:45:18 +0900

    start movie api prepare

--- src/App.js
+++ src/App.js
@@ -4,41 +4,19 @@ import PropTypes from 'prop-types';  // 유효성 체크 package
 // class component define
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log("hello");
+  state = {
+    isLoading: true,
+    movies: [],
   }
-  // class component 를 쓰는 이유. 상태가 있음
-  state = { // object
-    count: 0
-  };
-  add = () => {
-    // 새로운 state와 함께 render function이 호출되지 않음
-    // this.state.count = 1;  
-    this.setState(current => ({count : current.count + 1}));
-  };
-  minus = () => {
-    // this.state.count =-1;
-    // 현재 상태를 가지고 있을 수 있음(arrow function 활용)
-    this.setState(current => ({count : current.count - 1}));  
-  };
-
+  
   componentDidMount() {
-    console.log("component rendered");
-  }
-
-  componentDidUpdate() {
-    console.log("i justed update");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    console.log("render");
-    // render에서 뿌리려면 this로 접근
-    return (<div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
+    const { isLoading } = this.state.isLoading;
+    return <div>{isLoading ? "Loading...": "We are ready"}</div>
   }
 }
 
