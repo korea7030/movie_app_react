e48d817196716fd6549ee94c7d8485137a7a88df
korea7030 <korea7030@naver.com>
2020-04-18 14:25:49 +0900

    Updates

---
+++ precache-manifest.7556c979df3da63c3435b5095aad3632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bba58170986d83df3beda03479ab0d",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "a8e985fca3dfa28cb0bb",
+    "url": "/movie_app_react/static/css/main.20980125.chunk.css"
+  },
+  {
+    "revision": "0bcc86e7b2e3fca4c760",
+    "url": "/movie_app_react/static/js/2.fbf4c3cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_react/static/js/2.fbf4c3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e985fca3dfa28cb0bb",
+    "url": "/movie_app_react/static/js/main.1bfb9a07.chunk.js"
+  },
+  {
+    "revision": "fc4bb2ff0b90404674b8",
+    "url": "/movie_app_react/static/js/runtime-main.b9b89729.js"
+  }
+]);
